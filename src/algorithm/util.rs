439a4bcb77aa8061697478a397584c8e4acddd32
Ben <bencrulis@gmail.com>
2019-12-07 17:24:21 +0100

    modified ordering in case of NaN

--- src/algorithm/util.rs
+++ src/algorithm/util.rs
@@ -5,7 +5,7 @@ use std::cmp::Ordering;
 pub fn sorted_scores<V,P>(mut organisms: Vec<Organism<V>>, scorer: &dyn Scorer<V,P>, problem: &P) -> Vec<f64> {
 
     let mut scores: Vec<f64> = organisms.drain(..).map(|org| org.only_score(scorer, problem)).collect();
-    scores.sort_by(|x,y| y.partial_cmp(x).unwrap_or(Ordering::Less));
+    scores.sort_by(|x,y| y.partial_cmp(x).unwrap_or(Ordering::Greater));
 
     scores
 }
